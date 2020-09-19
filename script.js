f3b9db399ab9980a9ded38c00949c871c2ac78d0
maks <a@a.a>
2020-09-19 11:20:16 +0300

    pages

---
+++ script.js
@@ -0,0 +1,17 @@
+const main__form__button = document.querySelector('.main__form__button');
+const main__subscription__button = document.querySelector('.main__subscription__button');
+
+const pages = document.querySelectorAll('.page');
+
+main__form__button.addEventListener('click', () =>{
+    pages[1].scrollIntoView({
+        behavior: 'smooth'
+    })
+    
+
+} )
+main__subscription__button.addEventListener('click', () => {
+    pages[0].scrollIntoView({
+        behavior: 'smooth'
+    }) 
+})
