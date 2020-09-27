82af24a5bb14b6f80c8dc68e69b27bad14e7f3c9
maks <a@a.a>
2020-09-27 11:10:28 +0300

    modal

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const main__form__button = document.querySelector('.main__form__button');
 const main__subscription__button = document.querySelector('.main__subscription__button');
-
+const text = document.querySelector('.text');
 const pages = document.querySelectorAll('.page');
+const close = document.querySelector('.close');
+const translucent = document.querySelector('.translucent');
 
 main__form__button.addEventListener('click', () =>{
-    pages[1].scrollIntoView({
-        behavior: 'smooth'
-    })
-    
+    translucent.style.display = "block"
+    text.style.display = "flex";
+    text.classList.add("centered")
+
 
 } )
 main__subscription__button.addEventListener('click', () => {
@@ -16,7 +18,29 @@ main__subscription__button.addEventListener('click', () => {
     }) 
 })
 
+const closeModal = ()=>{
+    translucent.style.display = "none";
+    text.style.display ="none"
+}
+translucent.addEventListener('click', ()=>{
+    closeModal();
+})
 
+
+// DRY  -   don't repeat yourself
+
+text.addEventListener('click', ()=>{
+   
+
+})
+
+
+
+
+close.addEventListener('click',()=> {
+    closeModal();
+    
+})
 
 window.addEventListener( "resize", ()=>{
 
@@ -31,3 +55,9 @@ window.addEventListener( "resize", ()=>{
 
 
 })
+// window.addEventListener("wheel", (e) => {
+//     console.log(e.deltaY)
+//     pages[1].scrollIntoView({
+//         behavior: 'smooth'
+//     })
+// })
